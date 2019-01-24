8ebf731ed654599da5f6e0dd935cf2f77e016fd3
moolegovardhan <moole.govardhan@gmail.com>
2019-01-24 08:08:16 +0530

    class code

--- learning19.py
+++ learning19.py
@@ -17,5 +17,17 @@ print(set1.union(set2))
 #print(set1[1])
 
 #print(set1[0:3])
-
 print(unique_set)
+
+#union, intersection, sysmmetric_difference and normal difference
+set3 = {1, 2, 3, 4}
+set4 = {3, 4, 5, 6}
+print(set3.union(set4)) # returns the elements in both the sets but removes the duplicates
+
+print(set3.intersection(set4)) # this should return only the common elements in both the sets
+
+print(set3.symmetric_difference(set4)) # this will return all the elements except the common elements
+
+print(set3.difference(set4))
+
+print(set4.difference(set3))
---
+++ learning20.py
@@ -0,0 +1,64 @@
+# class - this is a blueprint of an object
+
+import random
+
+
+class Dice:
+
+    # this is the method executed when an object is created
+    # Shift+F6 is for renaming any variable, function or class anything - keyboard shortcut
+    def __init__(self):
+        print("creating of the object is done here")
+        # self.dice_face_up = 6;
+        # print(self)
+        # self.db_connection = db_connection
+        pass
+
+    def roll_dice(self):
+        self.dice_face_up = random.randint(1, 6)
+        # print(self)
+        pass
+
+    def do_another_thinG(self):
+        self.roll_dice()
+
+
+    # def get_data_from_db(self):
+        # self.db_connection.connect()
+
+print("creating an object")
+d = Dice() # this is where you are creating an object and assigining the object to d variable
+# print(d)
+# Dice d = new Dice();
+print("callig the roll_dice function: Iteration :1")
+d.roll_dice() # at this instance only self will have d object
+print(d.dice_face_up)
+
+print("callig the roll_dice function: Iteration :2")
+d.roll_dice()
+print(d.dice_face_up)
+
+print("callig the roll_dice function: Iteration :3")
+d.roll_dice()
+print(d.dice_face_up)
+
+# print("creating the second object")
+# d2 = Dice()
+# print(d2)
+# print("calling the roll_dice function for second object")
+# d2.roll_dice() # at this instance only self will have d2 object
+# print(d2.dice_face)
+
+# 1.
+
+
+# class Employee {
+#     // constructor
+#     public Employee(int noOfEmployees) {
+#         this.
+#         numberOfEmployee= noOfEmployees;
+#     }
+#     int numberOfEmployee;
+# }
+#
+# Employee d = new Employee(10);
